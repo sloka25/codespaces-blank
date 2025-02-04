9774c75d21d5567aef03fb4dcdab6d7745f1ef99
sloka25 <sloka.gandikota@gmail.com>
2025-02-04 14:08:14 -0800

    Update App.java

--- project euler/src/App.java
+++ project euler/src/App.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 public class App {
     public static void main(String[] args) throws Exception {
-        ArrayList<String> = new ArrayList<String>();
+        ArrayList<String> animals = new ArrayList<String>();
         
     }
 }
